470c2cf411f1e9400b7e216ac58b0cfb84c70896
Lawrence Brakmo <brakmo@fb.com>
2018-01-25 16:14:09 -0800

    UPSTREAM: bpf: Support passing args to sock_ops bpf function
    Adds support for passing up to 4 arguments to sock_ops bpf functions. It
    reusues the reply union, so the bpf_sock_ops structures are not
    increased in size.
    
    Signed-off-by: Lawrence Brakmo <brakmo@fb.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- net/ipv4/tcp_output.c
+++ net/ipv4/tcp_output.c
@@ -3591,7 +3591,8 @@ int tcp_connect(struct sock *sk)
 #ifdef CONFIG_HW_WIFIPRO
 	int wifipro_dev_max_len = 0;
 #endif
-	tcp_call_bpf(sk, BPF_SOCK_OPS_TCP_CONNECT_CB);
+
+	tcp_call_bpf(sk, BPF_SOCK_OPS_TCP_CONNECT_CB, 0, NULL);
 
 	if (inet_csk(sk)->icsk_af_ops->rebuild_header(sk))
 		return -EHOSTUNREACH; /* Routing failure or similar. */
