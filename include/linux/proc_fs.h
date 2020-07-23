64a19abbcded210d27562d352c41bc5276a2b1ee
Yonghong Song <yhs@fb.com>
2020-07-23 11:41:10 -0700

    UPSTREAM: bpf: Refactor to provide aux info to bpf_iter_init_seq_priv_t
    This patch refactored target bpf_iter_init_seq_priv_t callback
    function to accept additional information. This will be needed
    in later patches for map element targets since a particular
    map should be passed to traverse elements for that particular
    map. In the future, other information may be passed to target
    as well, e.g., pid, cgroup id, etc. to customize the iterator.
    
    Change-Id: Ie4194ce07d7cd10e1da29186ff8c286d3030a4f3
    Signed-off-by: Yonghong Song <yhs@fb.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Link: https://lore.kernel.org/bpf/20200723184110.590156-1-yhs@fb.com

--- include/linux/proc_fs.h
+++ include/linux/proc_fs.h
@@ -49,7 +49,8 @@ extern void sched_hwstatus_qos_dequeue(struct task_struct *task, int qos_type);
 extern void exit_proc_reclaim(struct task_struct *tsk);
 #endif
 
-extern int bpf_iter_init_seq_net(void *priv_data);
+struct bpf_iter_aux_info;
+extern int bpf_iter_init_seq_net(void *priv_data, struct bpf_iter_aux_info *aux);
 extern void bpf_iter_fini_seq_net(void *priv_data);
 
 #else /* CONFIG_PROC_FS */
