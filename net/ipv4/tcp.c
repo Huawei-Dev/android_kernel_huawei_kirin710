5e12f32256b90ce5b7c0773b9fc9b369258e80db
Huawei_Dev <surdu_petru@yahoo.com>
2026-04-11 18:45:17 +0200

    defconfig: remove unused flag CONFIG_WIFI_DELAY_STATISTIC

--- net/ipv4/tcp.c
+++ net/ipv4/tcp.c
@@ -297,10 +297,6 @@
 #include <hwnet/chr/chr_interface.h>
 #endif
 
-#ifdef CONFIG_WIFI_DELAY_STATISTIC
-#include <hwnet/ipv4/wifi_delayst.h>
-#endif
-
 #ifdef CONFIG_TCP_NODELAY
 #include <linux/blk-cgroup.h>
 #endif
@@ -1119,11 +1115,6 @@ new_segment:
 						  skb_queue_empty(&sk->sk_write_queue));
 			if (!skb)
 				goto wait_for_memory;
-#ifdef CONFIG_WIFI_DELAY_STATISTIC
-			if(DELAY_STATISTIC_SWITCH_ON) {
-				delay_record_first_combine(sk,skb,TP_SKB_DIRECT_SND,TP_SKB_TYPE_TCP);
-			}
-#endif
 			skb_entail(sk, skb);
 			copy = size_goal;
 		}
@@ -1515,11 +1506,6 @@ new_segment:
 						  first_skb);
 			if (!skb)
 				goto wait_for_memory;
-#ifdef CONFIG_WIFI_DELAY_STATISTIC
-			if(DELAY_STATISTIC_SWITCH_ON) {
-				delay_record_first_combine(sk,skb,TP_SKB_DIRECT_SND,TP_SKB_TYPE_TCP);
-			}
-#endif
 			process_backlog = true;
 #ifdef CONFIG_MPTCP
 			/*
@@ -2146,11 +2132,6 @@ int tcp_recvmsg(struct sock *sk, struct msghdr *msg, size_t len, int nonblock,
 				goto found_ok_skb;
 			if (TCP_SKB_CB(skb)->tcp_flags & TCPHDR_FIN)
 				goto found_fin_ok;
-#ifdef CONFIG_WIFI_DELAY_STATISTIC
-			if(DELAY_STATISTIC_SWITCH_ON) {
-				delay_record_rcv_combine(skb,sk,TP_SKB_TYPE_TCP);
-			}
-#endif
 			WARN(!(flags & MSG_PEEK),
 			     "TCP recvmsg seq # bug 2: copied %X, seq %X, rcvnxt %X, fl %X\n",
 			     *seq, TCP_SKB_CB(skb)->seq, tp->rcv_nxt, flags);
