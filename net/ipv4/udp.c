81a0bd47bd00ae967245c418459e668feca86d46
Huawei_Dev <surdu_petru@yahoo.com>
2026-04-11 20:23:44 +0200

    defconfig: remove unused flag CONFIG_HW_HIDATA_HIMOS

--- net/ipv4/udp.c
+++ net/ipv4/udp.c
@@ -124,10 +124,6 @@
 #include <huawei_platform/emcom/emcom_xengine.h>
 #endif
 
-#ifdef CONFIG_HW_HIDATA_HIMOS
-#include <huawei_platform/net/himos/hw_himos_udp_stats.h>
-#endif
-
 #ifdef CONFIG_MPTCP_EPC
 #include <net/mptcp_epc.h>
 #endif
@@ -816,9 +812,6 @@ static int udp_send_skb(struct sk_buff *skb, struct flowi4 *fl4)
 	int offset = skb_transport_offset(skb);
 	int len = skb->len - offset;
 	__wsum csum = 0;
-#ifdef CONFIG_HW_HIDATA_HIMOS
-	int skb_len = skb->len;
-#endif
 	/*
 	 * Create a UDP header
 	 */
@@ -861,9 +854,6 @@ send:
 	} else {
 		UDP_INC_STATS(sock_net(sk),
 			      UDP_MIB_OUTDATAGRAMS, is_udplite);
-#ifdef CONFIG_HW_HIDATA_HIMOS
-		himos_udp_stats(sk, 0, skb_len);
-#endif
 	}
 	return err;
 }
@@ -1708,9 +1698,6 @@ try_again:
 	if (!peeked) {
 		UDP_INC_STATS(sock_net(sk),
 			      UDP_MIB_INDATAGRAMS, is_udplite);
-#ifdef CONFIG_HW_HIDATA_HIMOS
-		himos_udp_stats(sk, ulen, 0);
-#endif
 	}
 
 	sock_recv_ts_and_drops(msg, sk, skb);
@@ -1934,9 +1921,6 @@ static int udp_queue_rcv_skb(struct sock *sk, struct sk_buff *skb)
 		encap_rcv = ACCESS_ONCE(up->encap_rcv);
 		if (encap_rcv) {
 			int ret;
-#ifdef CONFIG_HW_HIDATA_HIMOS
-			int len = skb->len;
-#endif
 			/* Verify checksum before giving to encap */
 			if (udp_lib_checksum_complete(skb))
 				goto csum_error;
@@ -1946,9 +1930,6 @@ static int udp_queue_rcv_skb(struct sock *sk, struct sk_buff *skb)
 				__UDP_INC_STATS(sock_net(sk),
 						UDP_MIB_INDATAGRAMS,
 						is_udplite);
-#ifdef CONFIG_HW_HIDATA_HIMOS
-				himos_udp_stats(sk, len, 0);
-#endif
 				return -ret;
 			}
 		}
--- net/ipv6/udp.c
+++ net/ipv6/udp.c
@@ -56,10 +56,6 @@
 #include <trace/events/skb.h>
 #include "udp_impl.h"
 
-#ifdef CONFIG_HW_HIDATA_HIMOS
-#include <huawei_platform/net/himos/hw_himos_udp_stats.h>
-#endif
-
 static bool udp6_lib_exact_dif_match(struct net *net, struct sk_buff *skb)
 {
 #if defined(CONFIG_NET_L3_MASTER_DEV)
@@ -440,9 +436,6 @@ try_again:
 		else
 			UDP6_INC_STATS(sock_net(sk), UDP_MIB_INDATAGRAMS,
 				       is_udplite);
-#ifdef CONFIG_HW_HIDATA_HIMOS
-		himos_udp_stats(sk, ulen, 0);
-#endif
 	}
 
 	sock_recv_ts_and_drops(msg, sk, skb);
@@ -630,9 +623,6 @@ static int udpv6_queue_rcv_skb(struct sock *sk, struct sk_buff *skb)
 		encap_rcv = ACCESS_ONCE(up->encap_rcv);
 		if (encap_rcv) {
 			int ret;
-#ifdef CONFIG_HW_HIDATA_HIMOS
-			int len = skb->len;
-#endif
 			/* Verify checksum before giving to encap */
 			if (udp_lib_checksum_complete(skb))
 				goto csum_error;
@@ -642,9 +632,6 @@ static int udpv6_queue_rcv_skb(struct sock *sk, struct sk_buff *skb)
 				__UDP_INC_STATS(sock_net(sk),
 						UDP_MIB_INDATAGRAMS,
 						is_udplite);
-#ifdef CONFIG_HW_HIDATA_HIMOS
-				himos_udp_stats(sk, len, 0);
-#endif
 				return -ret;
 			}
 		}
@@ -1076,9 +1063,6 @@ static int udp_v6_send_skb(struct sk_buff *skb, struct flowi6 *fl6)
 	__wsum csum = 0;
 	int offset = skb_transport_offset(skb);
 	int len = skb->len - offset;
-#ifdef CONFIG_HW_HIDATA_HIMOS
-	int skb_len = skb->len;
-#endif
 	/*
 	 * Create a UDP header
 	 */
@@ -1116,9 +1100,6 @@ send:
 	} else {
 		UDP6_INC_STATS(sock_net(sk),
 			       UDP_MIB_OUTDATAGRAMS, is_udplite);
-#ifdef CONFIG_HW_HIDATA_HIMOS
-		himos_udp_stats(sk, 0, skb_len);
-#endif
 	}
 	return err;
 }
