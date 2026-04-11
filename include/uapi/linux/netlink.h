f0834f39476c6b636b6abbe0f7fc540b1f460679
Huawei_Dev <surdu_petru@yahoo.com>
2026-04-11 20:29:10 +0200

    defconfig: remove CONFIG_HW_NETFILTER_MODULE

--- include/uapi/linux/netlink.h
+++ include/uapi/linux/netlink.h
@@ -49,47 +49,42 @@
 
 #define NETLINK_HW_LOGCAT_EXT 29
 
-/*netd communicate with hwfilter */
-#ifdef CONFIG_HW_NETFILTER_MODULE
-#define NETLINK_HW_NF   30
-#endif
-
 #if (defined(CONFIG_HW_CPULOAD_NOTI) || defined(CONFIG_HW_VIP_THREAD) \
         || defined(CONFIG_HW_IAWARE_THREAD_BOOST) || defined(CONFIG_HW_RTG_SCHED))
-#define NETLINK_HW_IAWARE_CPU 31
+#define NETLINK_HW_IAWARE_CPU 30
 #endif
 
 #ifdef CONFIG_RIL_NETLINK_MODULE
-#define NETLINK_RIL_EVENT_NL  32
+#define NETLINK_RIL_EVENT_NL  31
 #endif
 
 #ifdef CONFIG_HUAWEI_EMCOM
-#define NETLINK_EMCOM 33
+#define NETLINK_EMCOM 32
 #endif
 
 #ifdef CONFIG_RIL_SIM_NETLINK_MODULE
-#define NETLINK_RIL_EVENT_SIM  34
+#define NETLINK_RIL_EVENT_SIM  33
 #endif
 
 #ifdef CONFIG_HW_NETBOOSTER_MODULE
-#define NETLINK_NETBOOSTER  35
+#define NETLINK_NETBOOSTER  34
 #endif
 
 #ifdef CONFIG_HISI_CAMERA_VL53L0_970
-#define NETLINK_ST_LASER_DRIVER 36
+#define NETLINK_ST_LASER_DRIVER 35
 #endif
 
 #ifdef CONFIG_HUAWEI_EIMA_ACCESS_CONTROL
-#define NETLINK_EIMA 37
+#define NETLINK_EIMA 36
 #endif
 
 #ifdef CONFIG_HW_DC_MODULE
-#define NETLINK_HW_DC 38
+#define NETLINK_HW_DC 37
 #endif
 
-#define NETLINK_OLLIE 39
+#define NETLINK_OLLIE 38
 
-#define MAX_LINKS 40
+#define MAX_LINKS 39
 
 struct sockaddr_nl {
 	__kernel_sa_family_t	nl_family;	/* AF_NETLINK	*/
