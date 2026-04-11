7d053d7b21be3f04975fc4c8a19381f93bb6099d
Huawei_Dev <surdu_petru@yahoo.com>
2026-04-11 20:02:08 +0200

    defconfig: remove CONFIG_HW_NETWORK_AWARE

--- net/ipv4/tcp.c
+++ net/ipv4/tcp.c
@@ -287,9 +287,6 @@
 #ifdef CONFIG_HW_WIFIPRO
 #include <hwnet/ipv4/wifipro_tcp_monitor.h>
 #endif
-#ifdef CONFIG_HW_NETWORK_AWARE
-#include <network_aware/network_aware.h>
-#endif
 
 #ifdef CONFIG_TCP_NODELAY
 #include <linux/blk-cgroup.h>
@@ -1335,11 +1332,6 @@ int tcp_sendmsg_locked(struct sock *sk, struct msghdr *msg, size_t size)
 #ifdef CONFIG_HUAWEI_XENGINE
 	bool bAccelerate = false;
 #endif
-#ifdef CONFIG_HW_NETWORK_AWARE
-	tcp_network_aware(false);
-	stat_bg_network_flow(false, size);
-#endif
-
 	flags = msg->msg_flags;
 
 	if (flags & MSG_ZEROCOPY && size && sock_flag(sk, SOCK_ZEROCOPY)) {
@@ -2024,9 +2016,6 @@ int tcp_recvmsg(struct sock *sk, struct msghdr *msg, size_t len, int nonblock,
 
 	if (unlikely(flags & MSG_ERRQUEUE))
 		return inet_recv_error(sk, msg, len, addr_len);
-#ifdef CONFIG_HW_NETWORK_AWARE
-	tcp_network_aware(true);
-#endif
 
 	if (sk_can_busy_loop(sk) && skb_queue_empty(&sk->sk_receive_queue) &&
 	    (sk->sk_state == TCP_ESTABLISHED))
@@ -2263,9 +2252,6 @@ skip_copy:
 	tp->ops->cleanup_rbuf(sk, copied);
 #else
 	tcp_cleanup_rbuf(sk, copied);
-#endif
-#ifdef CONFIG_HW_NETWORK_AWARE
-	stat_bg_network_flow(true, copied);
 #endif
 	release_sock(sk);
 	return copied;
