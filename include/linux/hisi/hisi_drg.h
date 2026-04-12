31efbdebc0e28bd09a83c66bdd3dd8775ce81da6
Huawei_Dev <surdu_petru@yahoo.com>
2026-04-12 09:51:00 +0200

    defconfig: remove unused flag CONFIG_HISI_DRG

--- include/linux/hisi/hisi_drg.h
+++ include/linux/hisi/hisi_drg.h
@@ -7,21 +7,6 @@
 
 #define MAX_DRG_MARGIN		1024U
 
-#ifdef CONFIG_HISI_DRG
-extern void drg_cpufreq_register(struct cpufreq_policy *policy);
-extern void drg_cpufreq_unregister(struct cpufreq_policy *policy);
-extern void drg_devfreq_register(struct devfreq *df);
-extern void drg_devfreq_unregister(struct devfreq *df);
-extern void drg_cpufreq_cooling_update(unsigned int cpu,
-				       unsigned int clip_freq);
-extern void drg_devfreq_cooling_update(struct devfreq *df,
-				       unsigned long clip_freq);
-extern unsigned int drg_cpufreq_check_limit(struct cpufreq_policy *policy,
-					    unsigned int target_freq);
-extern unsigned long drg_devfreq_check_limit(struct devfreq *df,
-					     unsigned long target_freq);
-extern int drg_get_freq_range(struct drg_dev_freq *freq);
-#else
 static inline void drg_cpufreq_register(struct cpufreq_policy *policy) {}
 static inline void drg_cpufreq_unregister(struct cpufreq_policy *policy) {}
 static inline void drg_devfreq_register(struct devfreq *df) {}
@@ -40,7 +25,5 @@ static inline unsigned long drg_devfreq_check_limit(
 }
 
 extern int drg_get_freq_range(struct drg_dev_freq *freq) {return 0;}
-#endif
-
 extern bool hisi_cluster_cpu_all_pwrdn(void);
 #endif /* _HISI_DRG_H */
