6d19f5ddfdd79c218775199c59e086232591db61
Huawei_Dev <surdu_petru@yahoo.com>
2026-04-12 14:19:35 +0200

    defconfig: remove unused flag CONFIG_HUAWEI_PROC_CHECK_ROOT

--- mm/slub.c
+++ mm/slub.c
@@ -39,7 +39,6 @@
 
 #include <linux/hisi/rdr_hisi_ap_hook.h>
 #include <trace/events/kmem.h>
-#include <chipset_common/security/saudit.h>
 #include "internal.h"
 
 #ifdef CONFIG_HW_SLUB_DF
@@ -349,8 +348,6 @@ static inline bool hw_check_canary(struct kmem_cache *s, void *object, unsigned 
 	canary = hw_get_canary(s, object);
 
 	if (*canary == hw_get_canary_value(canary, value)) {
-		saudit_log(DOUBLE_FREE, STP_RISK, SAUDIT_ASYNC,
-			"type=harden,kmem_cache_name=%s", s->name);
 #ifdef CONFIG_HW_SLUB_DF_BUGON
 		BUG_ON(1);
 #endif
@@ -370,8 +367,6 @@ static inline bool hw_check_and_set_canary(struct kmem_cache *s, void *object,
 	canary = hw_get_canary(s, object);
 
 	if (*canary == hw_get_canary_value(canary, value)) {
-		saudit_log(DOUBLE_FREE, STP_RISK, SAUDIT_ASYNC,
-			"type=harden,kmem_cache_name=%s", s->name);
 #ifdef CONFIG_HW_SLUB_DF_BUGON
 		BUG_ON(1);
 #endif
@@ -3986,10 +3981,6 @@ err:
 	pr_err("ptr = %pK, page = %pK, n = %lu\n", ptr, page, n);
 	pr_err("page_addr = %pK, kmem_cache = %pK, size = %d, object= %lu, red_left_pad = %d",
 		page_address(page), s, s->size, object_size, s->red_left_pad);
-
-	saudit_log(USERCOPY, STP_RISK, 0,
-		"msg=kernel memory r/w attempt detected from/to %pK (%s) (%lu bytes),",
-		ptr, s->name, n);
 
 	BUG();
 }
