4d63b232890d5ebe551d141d138f3f4ab0c657b2
Huawei_Dev <surdu_petru@yahoo.com>
2026-04-12 13:44:14 +0200

    defconfig: remove/clean CONFIG_HUAWEI_HIDESYMS & CONFIG_HW_SLUB_SANITIZE

--- mm/slub.c
+++ mm/slub.c
@@ -245,18 +245,6 @@ static inline void stat(const struct kmem_cache *s, enum stat_item si)
 #endif
 }
 
-#ifdef CONFIG_HW_SLUB_SANITIZE
-static inline bool has_sanitize(struct kmem_cache *s)
-{
-#ifdef CONFIG_HW_SLUB_DF
-	return false;
-#else
-	return (s->flags & SLAB_CLEAR) &&
-		!(s->flags & (SLAB_TYPESAFE_BY_RCU | SLAB_POISON));
-#endif
-}
-#endif
-
 /********************************************************************
  * 			Core slab cache functions
  *******************************************************************/
@@ -312,19 +300,6 @@ static inline void set_freepointer(struct kmem_cache *s, void *object, void *fp)
 {
 	unsigned long freeptr_addr = (unsigned long)object + s->offset;
 
-#ifdef CONFIG_HW_SLUB_SANITIZE
-	if (unlikely(object == fp)) {
-#ifdef CONFIG_HW_SLUB_DF
-		set_harden_double_free_check_flags(true);
-#else
-		s->flags |= SLAB_CLEAR;
-#endif
-		WARN_ON(1);
-		saudit_log(DOUBLE_FREE, STP_RISK, SAUDIT_ASYNC,
-			"type=light,kmem_cache_name=%s", s->name);
-		return;
-	}
-#endif
 	*(void **)freeptr_addr = freelist_ptr(s, fp, freeptr_addr);
 }
 
@@ -2940,10 +2915,6 @@ static void __slab_free(struct kmem_cache *s, struct page *page,
 		prior = page->freelist;
 		counters = page->counters;
 		set_freepointer(s, tail, prior);
-#ifdef CONFIG_HW_SLUB_SANITIZE
-		if (unlikely(get_freepointer(s, tail) != prior))
-			return;
-#endif
 		new.counters = counters;
 		was_frozen = new.frozen;
 		new.inuse -= cnt;
@@ -3054,21 +3025,6 @@ static __always_inline void do_slab_free(struct kmem_cache *s,
 	void *tail_obj = tail ? : head;
 	struct kmem_cache_cpu *c;
 	unsigned long tid;
-#ifdef CONFIG_HW_SLUB_SANITIZE
-	if (unlikely(has_sanitize(s))) {
-		int offset = s->offset ? 0 : sizeof(void *);
-		void *x = head;
-
-		while (1) {
-			memset(x + offset, 0, s->object_size - offset);
-			if (s->ctor)
-				s->ctor(x);
-			if (x == tail_obj)
-				break;
-			x = get_freepointer(s, x);
-		}
-	}
-#endif
 
 #ifdef CONFIG_HW_SLUB_DF
 	if (unlikely(s->flags & SLAB_DOUBLEFREE_CHECK) &&
@@ -3093,10 +3049,6 @@ redo:
 
 	if (likely(page == c->page)) {
 		set_freepointer(s, tail_obj, c->freelist);
-#ifdef CONFIG_HW_SLUB_SANITIZE
-	if (unlikely(get_freepointer(s, tail_obj) != c->freelist))
-		return;
-#endif
 		if (unlikely(!this_cpu_cmpxchg_double(
 				s->cpu_slab->freelist, s->cpu_slab->tid,
 				c->freelist, tid,
