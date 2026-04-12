498936563a2d22ba34591694f414e0236388fa6b
Huawei_Dev <surdu_petru@yahoo.com>
2026-04-12 12:11:23 +0200

    fs/proc: move hwioinfo.c to hisi/

--- include/linux/page-flags-layout.h
+++ include/linux/page-flags-layout.h
@@ -82,7 +82,6 @@
 #define LAST_CPUPID_WIDTH 0
 #endif
 #define PROTECT_LRU_WIDTH ({ BUILD_BUG(); 0; })
-#endif
 /*
  * We are going to use the flags for the page to node mapping if its in
  * there.  This includes the case where there is no node, so it is implicit.
